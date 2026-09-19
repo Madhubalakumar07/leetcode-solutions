1be63c7e68d5afcae97d3d2e834ef3d4db2b82ea
MADHUBALAKUMAR S <madhubalakumar7.25@gmail.com>
2026-09-19 20:58:41 +0530

    Time: 7 ms (66.65%), Space: 43.9 MB (85.25%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        if(len1 > len2) return false;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        for(int i = 0; i<len1; i++){
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+        if(Arrays.equals(freq1, freq2)) return true;
+        for(int i = len1; i<len2; i++){
+            freq2[s2.charAt(i) - 'a']++;
+            freq2[s2.charAt(i-len1) - 'a']--;
+            if(Arrays.equals(freq1, freq2)) return true;
+        }
+        return false;
+    }
+}
